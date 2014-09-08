a10ce668e02ff3437164a01066c4d07cd072c7f8
Alexey Stukalov <astukalov@cemm.oeaw.ac.at>
2014-09-08 20:04:42 +0200

    testDAU(): faster AA group frequencies calculation
    streamline AA to AA group conversion and make it easily extendable to
    other AA classifications

--- R/testDAU.R
+++ R/testDAU.R
@@ -17,82 +17,66 @@ testDAU <- function(dagPeptides, dagBackground,
             "Ile"="I", "Leu"="L", "Lys"="K", "Met"="M", "Phe"="F", 
             "Pro"="P", "Ser"="S", "Thr"="T", "Trp"="W", 
             "Tyr"="Y", "Val"="V")
-    classic <- list("nonpolar_aliphatic"=c("A", "G", "L", "M", "I", "V"),
+    AA_identity <- factor( AA )
+    names( AA_identity ) <- AA
+
+    aa_map <- function( class_to_aa ) {
+      AA_classes <- factor( unlist( lapply( names( class_to_aa ), function( aa_class ) {
+        rep.int( aa_class, length( class_to_aa[[aa_class]] ) )
+      } ) ) )
+      names( AA_classes ) <- unlist( class_to_aa )
+      return ( AA_classes )
+    }
+    
+    groups <- lapply( list(
+    classic = list("nonpolar_aliphatic"=c("A", "G", "L", "M", "I", "V"),
                  "polar_uncharged"=c("C", "P", "Q", "S", "T"),
                  "aromatic"=c("F", "W", "Y"),
                  "positively_charged"=c("H", "K", "N", "R"),
-                 "negatively_charged"=c("D", "E"))
-    charge <- list("positive"=c("H", "K", "R"),
+                 "negatively_charged"=c("D", "E")),
+    charge = list("positive"=c("H", "K", "R"),
                 "neutral"=c("A", "C", "F", "G", "I", "L", "M", "N", "P", "Q",
                             "S", "T", "V", "W", "Y"),
-                "negative"=c("D", "E"))
-    chemistry <- list("hydrophobic"=c("A", "F", "I", "L", "M", "P", "V", "W"),
+                "negative"=c("D", "E")),
+    chemistry = list("hydrophobic"=c("A", "F", "I", "L", "M", "P", "V", "W"),
                    "polar"=c("C", "G", "S", "T", "Y"),
                    "basic"=c("H", "K", "R"),
                    "neutral"=c("N", "Q"),
-                   "acidic"=c("D", "E"))
-    hydrophobicity <- list("hydrophilic"=c("D", "E", "K", "N", "Q", "R"), 
+                   "acidic"=c("D", "E")),
+    hydrophobicity = list("hydrophilic"=c("D", "E", "K", "N", "Q", "R"), 
                         "neutral"=c("A", "G", "H", "P", "S", "T"), 
-                        "hydrophobic"=c("C", "F", "I", "L", "M", "V", "W", "Y"))
-    coln <- if(group=="null") as.character(AA) else names(get(group))
-    ##convert by group
-    convert <- function(x, gtype){
-        d <- dim(x)
-        x <- as.character(x)
-        for(i in 1:length(gtype)){
-            id <- x %in% gtype[[i]]
-            name <- names(gtype)[i]
-            x[id] <- name
-        }
-        matrix(x, nrow=d[1], ncol=d[2])
-    }
-    groupAA <- function(dat, group){
-        dat <- switch(group,
-                      classic=convert(dat, classic),
-                      charge=convert(dat, charge),
-                      hydrophobicity=convert(dat, hydrophobicity),
-                      chemistry=convert(dat, chemistry),
-                      null=dat
-        )
-        dat
-    }
-    bg <- lapply(bg, function(.bg) groupAA(.bg, group))
-    exp <- groupAA(exp, group)
+                        "hydrophobic"=c("C", "F", "I", "L", "M", "V", "W", "Y")),
+    null = AA_identity 
+    ), aa_map )
+    
     if(ncol(exp)!=ncol(bg[[1]])){
         stop("the length of background is different from inputs", call.=FALSE)
     }
-    counts <- function(mat, coln){
-        num <- apply(mat, 2, function(.ele){
-            cnt <- table(.ele)
-            cnt <- cnt[names(cnt) %in% coln]
-            total <- sum(cnt)
-            percentage <- cnt/total
-            percentage[coln]
-        })
+    group_freqs <- function(x,gtype){
+        df <- subset( data.frame( x = groups[[gtype]][as.character(x)],
+                    col = rep( seq_len(ncol(x)), each = nrow(x) ) ), !is.na(x) )
+        res <- table( df$x, df$col )
+        as.matrix( as.data.frame.matrix( sweep( res, 2, colSums( res ), "/" ) ) )
     }
-    bg <- lapply(bg, counts, coln)
-    exp <- counts(exp, coln)
-    rownames(exp) <- coln
-    bg <- lapply(1:ncol(exp), function(i){
-        do.call(cbind, lapply(bg, function(.bg){ .bg[,i]}))
+    bg_freqs <- lapply(bg, group_freqs, group)
+    exp_freqs <- group_freqs(exp, group)
+    bg_col_freqs <- lapply(seq_len(ncol(exp_freqs)), function(i){
+        do.call(cbind, lapply(bg_freqs, function(.bg){ .bg[,i] }))
     })
     ##Z-score = (x-mu)/std
-    std <- do.call(cbind, lapply(bg, function(.bg){
+    bg_sd <- do.call(cbind, lapply(bg_col_freqs, function(.bg){
         apply(.bg, 1, sd, na.rm=TRUE)
     }))
-    mu <- do.call(cbind, lapply(bg, function(.bg){
-       apply(.bg, 1, mean, na.rm=TRUE) 
+    bg_mu <- do.call(cbind, lapply(bg_col_freqs, function(.bg){
+       rowMeans(.bg, na.rm=TRUE)
     }))
     ##difference
-    exp[is.na(exp)] <- 0
-    diff <- exp - mu
+    exp_freqs[is.na(exp_freqs)] <- 0
+    diff <- exp_freqs - bg_mu
     diff[is.na(diff)] <- 0
     
-    zscore <- diff/std
-    
-    rownames(diff) <- coln
-    rownames(zscore) <- coln
-    coln <- c()
+    zscore <- diff/bg_sd
+
     if(dagPeptides@upstreamOffset>0){
         coln <- paste("AA", -1*(dagPeptides@upstreamOffset:1), sep="")
     }
@@ -110,7 +94,7 @@ testDAU <- function(dagPeptides, dagBackground,
                    difference=diff,
                    zscore=zscore,
                    pvalue=pvalue,
-                   background=mu,
+                   background=bg_mu,
                    motif=exp,
                    upstream=dagPeptides@upstreamOffset,
                    downstream=dagPeptides@downstreamOffset)
